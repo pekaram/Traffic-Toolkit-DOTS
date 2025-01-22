7aa9d76ab19d8b3d7fc3b6c10c17068cb6eebdff
Peter <petertalaat@hotmail.com>
2025-01-22 21:22:21 +0200

    fix: [MTS-20] vague arrow appearing when selecting TrafficLight in editor

--- Assets/Scripts/Editing Tools/TrafficLightEditor.cs
+++ Assets/Scripts/Editing Tools/TrafficLightEditor.cs
@@ -16,16 +16,6 @@ public class TrafficLightEditor : Editor
     private void OnSceneGUI()
     {
         Visualize(_trafficLight);
-
-        if (_trafficLight.Lane?.Waypoints == null || _trafficLight.Lane.Waypoints.Count < 1)
-            return;
-
-        Handles.color = Color.red;
-        var firstWaypoint = _trafficLight.Lane.Waypoints[0];
-        var lastWaypoint = _trafficLight.Lane.Waypoints[^1];
-        var arrowHeadRotation = Quaternion.LookRotation(lastWaypoint - firstWaypoint, Vector3.up);
-        Handles.ArrowHandleCap(0, lastWaypoint, arrowHeadRotation, 4, EventType.Repaint);
-
     }
 
     public static void Visualize(TrafficLightAuthoring trafficLight)

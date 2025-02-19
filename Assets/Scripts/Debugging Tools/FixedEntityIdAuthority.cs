f7b6354f6f2b154c0010284ece78531219cedc62
Peter <petertalaat@hotmail.com>
2025-02-19 13:04:17 +0200

    [MTS-32] Universal Runtime ID for Vehicles

---
+++ Assets/Scripts/Debugging Tools/FixedEntityIdAuthority.cs
@@ -0,0 +1,23 @@
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+using System;
+
+public class FixedEntityIdAuthority : MonoBehaviour
+{
+}
+
+public class FixedEntityIdBaker : Baker<FixedEntityIdAuthority> 
+{
+    public override void Bake(FixedEntityIdAuthority authoring)
+    {
+        var entity = GetEntity(TransformUsageFlags.None);
+
+        AddComponent(entity, new FixedEntityId { Id = Guid.NewGuid().ToString() });
+    }
+}
+
+public struct FixedEntityId : IComponentData
+{
+    public FixedString64Bytes Id;
+}
